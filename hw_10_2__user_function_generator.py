3d0ad84281e35788a29a1219c8aed1f4eaeea161
Ihor Kyrkach <i.kyrkach@steel.mil.ua>
2024-07-15 17:24:28 +0300

    HW #10.2: some_gen(begin, end, func) - generator that starts with begin value followed by (end - 1) values of passed function func(), starting from func(begin)

---
+++ hw_10_2__user_function_generator.py
@@ -0,0 +1,17 @@
+from inspect import isgenerator
+
+
+def pow(x):
+    return x ** 2
+
+
+def some_gen(begin, end, func):
+    for i in range(end):
+        yield begin
+        begin = func(begin)
+
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
